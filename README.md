f319a7920b7f9197f3213d22ba826e02c7e2637c
Ulfangur <ulfangur@gmail.com>
2026-03-23 16:33:48 +0100

    INSTRUCTION.md dans map pour expliquer comment fonctionnera la map coté code. modification légère d'une ligne Troupes.h et cpp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Wargame
+
+On a décidé d'aller pointe vers le haut pour les hexagone
--- src/troupes/Troupes.h
+++ src/troupes/Troupes.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 
 enum Equipe {
     Default,
@@ -6,28 +8,26 @@ enum Equipe {
     Bleu
 };
 
+enum Allowed_terrain{//A changer pour quand j'aurai Enum class venant de terrain
+    Water,
+    Mountain,
+    Desert,
+    Snow,
+    Coast,
+    Grassland
+};
+
 class Troupes{
     public:
-        Troupes(int pv, int vitesse, int force, int def):_pv(pv),_vitesse(vitesse),_force(force),_def(def),_portee(0),_ekip(Equipe(Default)){};
+        Troupes(int pv, int vitesse, int force, int def):_pv(pv),_vitesse(vitesse),_force(force),_def(def),_portee(0),_ekip(Default){};
         ~Troupes(){}
-        void attaque(Troupes *ennemy){
-            if (ennemy->get_ekip() != _ekip){
-                ennemy->change_pv(_force);
-                this->change_pv(ennemy->get_force());
-                if (ennemy->get_pv() <= 0){
-                    delete ennemy;
-                }
-                if (_pv <= 0){
-                    delete this;
-                }
-            }
-        }
+        void attaque(Troupes *ennemy){}
 
         Equipe get_ekip() {return _ekip;}
         int get_pv(){return _pv;}
         int get_force(){return _force;}
         void change_pv(int atq) {_pv -= atq;}
-
+        int get_def(){return _def;}
     private:
         Equipe _ekip;
         int _pv; 
@@ -35,25 +35,30 @@ class Troupes{
         int _force; 
         int _def; 
         int _portee ; //Portée d'attaque de Troupes
-    
+    protected:
+        std::vector<Allowed_terrain> Allow_terrain; // 
+        void set_allow_terrain(std::vector<Allowed_terrain> terrains) 
+        {Allow_terrain = terrains;};
 };
 
 class Terrestre : public Troupes{
     public:
-        Terrestre(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def){};
+        Terrestre(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def)
+        {set_allow_terrain({Desert,Snow,Coast,Grassland});};
     private:
-
 };
 
 
 class Maritime : public Troupes{
     public:
-        Maritime(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def){};
+        Maritime(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def)
+        {set_allow_terrain({Water});};
     private:
 };
 
 class Aerienne : public Troupes{
     public:
-        Aerienne(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def){};
+        Aerienne(int pv, int vitesse, int force, int def):Troupes(pv,vitesse,force,def)
+        {set_allow_terrain({Desert,Snow,Coast,Grassland,Water});};
     private:
 };
