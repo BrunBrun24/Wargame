ddb7ed3bcc35d89f849fe5e244d9369d4106f6ca
BrunBrun <aubrunpaul49@gmail.com>
2026-03-25 22:40:36 +0100

    Ajout dans la classe "Unit" la possibilité de se mettre en garde et de savoir si l'unité peut se déplacer

--- src/units/units.cpp
+++ src/units/units.cpp
@@ -8,39 +8,39 @@
 
 const std::map<UnitName, Stats> unitData = {
     // Terrestre
-    {UnitName::Warrior, {100, 20, 20, 2, 1}},
-    {UnitName::Swordsman, {120, 35, 35, 2, 1}},
-    {UnitName::Musketman, {150, 55, 50, 2, 1}},
-    {UnitName::Infantry, {180, 75, 70, 3, 1}},
-    {UnitName::MechanizedInfantry, {220, 85, 85, 4, 1}},
+    {UnitName::Warrior, {100, 20, 2, 1}},
+    {UnitName::Swordsman, {120, 35, 2, 1}},
+    {UnitName::Musketman, {150, 55, 2, 1}},
+    {UnitName::Infantry, {180, 75, 3, 1}},
+    {UnitName::MechanizedInfantry, {220, 85, 4, 1}},
 
     // A distance
-    {UnitName::Archer, {80, 15, 10, 2, 2}},
-    {UnitName::Crossbowman, {100, 30, 20, 2, 2}},
-    {UnitName::FieldCannon, {130, 50, 40, 2, 2}},
-    {UnitName::MachineGun, {160, 70, 65, 2, 1}},
+    {UnitName::Archer, {80, 15, 2, 2}},
+    {UnitName::Crossbowman, {100, 30, 2, 2}},
+    {UnitName::FieldCannon, {130, 50, 2, 2}},
+    {UnitName::MachineGun, {160, 70, 2, 1}},
 
     // Cavalerie
-    {UnitName::Horseman, {110, 36, 30, 4, 1}},
-    {UnitName::Knight, {140, 48, 45, 4, 1}},
-    {UnitName::Cuirassier, {170, 65, 60, 4, 1}},
-    {UnitName::Tank, {250, 80, 70, 4, 1}},
-    {UnitName::ModernArmor, {300, 90, 85, 5, 1}},
+    {UnitName::Horseman, {110, 36, 4, 1}},
+    {UnitName::Knight, {140, 48, 4, 1}},
+    {UnitName::Cuirassier, {170, 65, 4, 1}},
+    {UnitName::Tank, {250, 80, 4, 1}},
+    {UnitName::ModernArmor, {300, 90, 5, 1}},
 
     // Naval
-    {UnitName::Galley, {120, 30, 30, 3, 1}},
-    {UnitName::Caravel, {160, 50, 50, 4, 1}},
-    {UnitName::Ironclad, {220, 70, 65, 4, 1}},
-    {UnitName::Destroyer, {280, 85, 80, 5, 1}},
-    {UnitName::Submarine, {150, 75, 60, 4, 3}},
-    {UnitName::AircraftCarrier, {350, 65, 80, 3, 1}},
+    {UnitName::Galley, {120, 30, 3, 1}},
+    {UnitName::Caravel, {160, 50, 4, 1}},
+    {UnitName::Ironclad, {220, 70, 4, 1}},
+    {UnitName::Destroyer, {280, 85, 5, 1}},
+    {UnitName::Submarine, {150, 75, 4, 3}},
+    {UnitName::AircraftCarrier, {350, 65, 3, 1}},
 
     // Aérienne
-    {UnitName::Biplane, {100, 60, 55, 6, 3}},
-    {UnitName::Fighter, {140, 80, 75, 8, 4}},
-    {UnitName::JetFighter, {180, 95, 90, 10, 5}},
-    {UnitName::Bomber, {150, 85, 70, 7, 6}},
-    {UnitName::JetBomber, {200, 110, 85, 9, 8}}};
+    {UnitName::Biplane, {100, 60, 6, 3}},
+    {UnitName::Fighter, {140, 80, 8, 4}},
+    {UnitName::JetFighter, {180, 95, 10, 5}},
+    {UnitName::Bomber, {150, 85, 7, 6}},
+    {UnitName::JetBomber, {200, 110, 9, 8}}};
 
 const StrengthWeaknessMatrix unit_strength_weakness_matrix =
     UnitParser::load_strength_weakness_matrix();
@@ -52,11 +52,11 @@ Unit::Unit(UnitName name, Country country,
     : _name(name),
       _country(country),
       _stats(unitData.at(name)),
-      allow_terrain(allow_terrain) {
+      allow_terrain(allow_terrain),
+      _travel_possible(true),
+      _on_guard(false) {
   this->_id = _id_counter++;
 }
-
-Unit::~Unit() {}
 
 void Unit::attack(Unit& ennemy) {
   // On attaque l'ennemie
@@ -74,7 +74,7 @@ bool Unit::find_terrain(const TerrainsType& target_terrain) const {
   return it != allow_terrain.end();
 }
 
-int Unit::calculate_damage(Unit& ennemy) const {
+int Unit::calculate_damage(const Unit& ennemy) const {
   // Si l'unité est affaiblie, elle fait moins de dégâts
   double percentage_hp_remaining =
       static_cast<double>(_stats.hp) / unitData.at(this->get_name()).hp;
@@ -88,5 +88,20 @@ int Unit::calculate_damage(Unit& ennemy) const {
   double raw_damage =
       _stats.power * percentage_hp_remaining * (1.0 + strength_weakness);
 
+  // Si l'unité est en garde alors la puissance de l'attaque prend -30%
+  if (ennemy.get_on_guard()) {
+    raw_damage *= 0.7;
+  }
+
   return static_cast<int>(raw_damage);
+}
+
+void Unit::heal() {
+  int max_hp = unitData.at(_name).hp;
+
+  if (_stats.hp < max_hp) {
+    int amount_to_heal = static_cast<int>(max_hp * 0.2);
+
+    _stats.hp = std::min<int>(_stats.hp + amount_to_heal, max_hp);
+  }
 }
--- src/units/units.h
+++ src/units/units.h
@@ -4,21 +4,8 @@
 #include <map>
 #include <vector>
 
+#include "player.h"
 #include "terrains.h"
-
-enum class Country {
-  None,
-  France,
-  Germany,
-  UnitedKingdom,
-  Russia,
-  Egypt,
-  Switzerland,
-  Japan,
-  Spain,
-  Italy,
-  UnitedStates
-};
 
 enum class UnitName {
   // Terrestre
@@ -60,7 +47,6 @@ enum class UnitName {
 struct Stats {
   int hp;
   int power;
-  int defense;
   int speed;
   int range;
 };
@@ -73,11 +59,13 @@ extern const StrengthWeaknessMatrix unit_strength_weakness_matrix;
 class Unit {
  public:
   Unit(UnitName name, Country country, std::vector<TerrainsType> allow_terrain);
-  ~Unit();
 
-  int calculate_damage(Unit& ennemy) const;
+  int calculate_damage(const Unit& ennemy) const;
   void attack(Unit& ennemy);
   bool find_terrain(const TerrainsType& target_terrain) const;
+  void switch_travel_possible() { _travel_possible = !_travel_possible; }
+  void switch_on_guard() { _on_guard = !_on_guard; }
+  void heal();
 
   int get_id() const { return _id; }
   std::vector<TerrainsType> get_allow_terrain() const { return allow_terrain; }
@@ -85,6 +73,8 @@ class Unit {
   UnitName get_name() const { return _name; }
   Country get_country() const { return _country; }
   int get_speed() const { return _stats.speed; }
+  bool get_travel_possible() const { return _travel_possible; }
+  bool get_on_guard() const { return _on_guard; }
 
  protected:
   std::vector<TerrainsType> allow_terrain;
@@ -96,4 +86,6 @@ class Unit {
   UnitName _name;
   Country _country;
   Stats _stats;
+  bool _travel_possible;  // par default à true quand l'unité apparaît
+  bool _on_guard;         // par default à false quand l'unité apparaît
 };
