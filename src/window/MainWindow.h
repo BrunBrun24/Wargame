cfca3080208a3625b8238d1a49e347ad3e0a3504
Ulfangur <130994158+Ulfangur@users.noreply.github.com>
2026-03-30 11:03:22 +0200

    Add files via upload

---
+++ src/window/MainWindow.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class MainWindow {
+    //si on est sur linux 
+    //si on est sur Windows
+    //si on est sur autre chose on vas se faire foutre
+    public:
+
+    private:
+}
