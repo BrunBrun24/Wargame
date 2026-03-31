04823ff07e98ce3778955106f9f807df9f748e31
Ulfangur <ulfangur@gmail.com>
2026-03-31 07:59:35 +0200

    Ajout de OpenGL et GLFW : changement du cmakelist pour ajouter ces deux éléments. main.cpp montre un exemple d'utilisation. J'ai ajouté au cap ou les fichiers zip si on a un problème avec ces outils.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "map.h"
-#include "terrestrial.h"
-#include "units.h"
-
 int main() {
-  Map map;
-  map.create_map();
-//   map.render_debug();
+    // Initialiser GLFW
+    glfwInit();
 
-  Terrestrial my_warrior(UnitName::Archer, Country::France, 100, 2, 20, 10, 1);
-  my_warrior.get_unit_advantages();
+    // Version OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//version de opengl (3)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//version de opengl après le point (3.X)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//Aucune idée pour le moment
 
-  std::cout << "Good Execution !";
-  return 0;
+    // Créer fenêtre 
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);  // Width height title IDK IDK 
+    if (!window) {
+        std::cout << "Erreur fenêtre\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Charger OpenGL avec GLAD
+    // Glad etant Ce qui permet a opengl de fonctionner plus facilement.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Erreur GLAD\n";
+        return -1;
+    }
+
+    // Boucle principale
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
